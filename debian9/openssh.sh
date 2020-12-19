6963cd02a09b2a138f8bd40612cc2cd9c756b83d
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2020-12-20 06:22:59 +0700

    Update openssh.sh

--- debian9/openssh.sh
+++ debian9/openssh.sh
@@ -307,7 +307,7 @@ echo 'Please send in your comments and/or suggestions to zaf@vsnl.com'
 # download script
 cd /usr/bin
 wget -O menu "https://raw.githubusercontent.com/idtunnel/sshtunnel/master/debian9/menu.sh"
-wget -O usernew "https://raw.githubusercontent.com/idtunnel/sshtunnel/master/debian9/usernew.sh"
+wget -O usernew "https://raw.githubusercontent.com/fisabiliyusri/sulaimanssh/master/debian9/usernew.sh"
 wget -O trial "https://raw.githubusercontent.com/idtunnel/sshtunnel/master/debian9/trial.sh"
 wget -O hapus "https://raw.githubusercontent.com/idtunnel/sshtunnel/master/debian9/hapus.sh"
 wget -O cek "https://raw.githubusercontent.com/idtunnel/sshtunnel/master/debian9/user-login.sh"
