cbcea4ff2b2cfa9ace35ba67cc68d48749d15c67
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2020-12-21 05:58:29 +0700

    Update openssh.sh

--- debian9/openssh.sh
+++ debian9/openssh.sh
@@ -407,10 +407,10 @@ echo "========================================================="
 /etc/init.d/ssh restart
 /etc/init.d/dropbear restart
 /etc/init.d/stunnel4 restart
-#/etc/init.d/squid restart
+# /etc/init.d/squid restart
 /etc/init.d/nginx restart
-#/etc/init.d/php5.6-fpm restart
-#/etc/init.d/openvpn restart
+# /etc/init.d/php5.6-fpm restart
+# /etc/init.d/openvpn restart
 
 # Delete script
 #rm -f /root/openvpn.sh
